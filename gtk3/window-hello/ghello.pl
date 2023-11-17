e5ccded1e55175162f6e4fe6350f110e44b8fc51
v02qabs <takesue090@gmail.com>
2023-11-18 05:19:13 +0900

    gtk3-button-add-textview-box-layout

--- gtk3/window-hello/ghello.pl
+++ gtk3/window-hello/ghello.pl
@@ -1,8 +1,13 @@
 
 use Gtk3 -init;
 my $window = Gtk3::Window->new ('toplevel');
+my $textarea = Gtk3::TextView->new();
+
 my $button = Gtk3::Button->new ('Quit');
 $button->signal_connect (clicked => sub { Gtk3::main_quit });
-$window->add ($button);
+my $box = Gtk3::Box->new('vertical',2);
+$box->pack_start($textarea, FALSE, FALSE,0);
+$box->pack_start($button, FALSE, FALSE,0);
+$window->add($box);
 $window->show_all;
 Gtk3::main;
